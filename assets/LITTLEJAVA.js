7e14bb998e5ecc4d53d1400c12ea8d45fa1097b5
jao1224 <107001117+jao1224@users.noreply.github.com>
2023-11-20 10:18:28 -0300

    Add files via upload

--- assets/LITTLEJAVA.js
+++ assets/LITTLEJAVA.js
@@ -1,47 +1,98 @@
-window.onload=function() {
-    const button = document.querySelector('#btn-processar');
-    button.addEventListener("click", transicao)
-}
-
-
-function transicao() {
-    document.querySelector('#area1').classList.add('fade1')
-    document.querySelector('#area2').classList.add('fade2')
-    document.querySelector('#area3').classList.add('fade3')
-
-}
-
 function processar () {
-    const {peso, altura, idade} = pegarDados();
+  const {peso, altura, idade} = pegarDados();
+  const isValid = verificador(peso, altura, idade);
+  if (!isValid) {
+      return;
+  }
+  const {calculoIMC} = calcIMC(peso, altura);
+  const {precoAPlano1, precoAPlano2, precoAPlano3} = calculoPlanosA(idade, calculoIMC);
+  const {precoBPlano1, precoBPlano2, precoBPlano3} = precosBPlanos(calculoIMC);
+  const {melhorOP1, melhorOP2, melhorOP3} = compararPrecos(precoAPlano1, precoAPlano2, precoAPlano3, precoBPlano1, precoBPlano2, precoBPlano3);
+  exibirResultado(
+  melhorOP1.toFixed(2),
+  melhorOP2.toFixed(2),
+  melhorOP3.toFixed(2));
+  transicao()
+  scrollar();
+  scrollON();
+
+ var resultado = document.getElementById("resultado"); // Seleciona a div com id="resultado"
+  resultado.style.display = "block"; // Altera o estilo da div para exibir
+  var operadora = ""; // Cria uma variável para armazenar a operadora recomendada
+  var plano = ""; // Cria uma variável para armazenar o plano recomendado
+  // Compara os preços dos planos e atribui os valores às variáveis
+  if (melhorOP1 < melhorOP2 && melhorOP1 < melhorOP3) {
+      operadora = "A";
+      plano = "1";
+  } else if (melhorOP2 < melhorOP1 && melhorOP2 < melhorOP3) {
+      operadora = "B";
+      plano = "2";
+  } else {
+      operadora = "C";
+      plano = "3";
+  }
+  resultado.innerHTML = `A operadora recomendada é ${operadora} e o plano recomendado é ${plano}.`;
+}
+
+function scrollar() {
+    var {coordenadas} = distaciaDoTopo();
+    const posicaoY = (coordenadas.y);
+    window.scroll({
+        top: posicaoY - 126,
+        behavior: "smooth"
+    })
+}
+
+function distaciaDoTopo() {
+    const PlanoBasicoBox = document.getElementById('area1');
+    var coordenadas = PlanoBasicoBox.getBoundingClientRect();
+    return {coordenadas}
+
+}
+
+function verificador(peso, altura, idade){
+    if (peso == "", altura == "", idade == "" ) {
+        Swal.fire({
+            title: "Erro",
+            text: "Por favor, preencha todos os campos.",
+            icon: "error",
+            confirmButtonColor: "#0d6efd"
+          });
+        return false;
+
+    }
+    return true;
+}
+
+
+
+function irTopo() {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    })
+}
+
+function scrollON() {
+    document.querySelector('#corpo-pagina').classList.remove('scrolloff');
+}
+function calcIMC(peso, altura) {
     const calculoIMC = peso / (altura ** 2);
+    return {calculoIMC};
+}
+
+function calculoPlanosA(idade, calculoIMC) {
     const precoAPlano1 = 100 + (idade * 10 * (calculoIMC / 10));
     const precoAPlano2 = (150 + (idade * 15)) * (calculoIMC / 10);
     const precoAPlano3 = (200 - (calculoIMC * 10) + (idade * 20)) * (calculoIMC / 10);
-    const {precoBPlano1, precoBPlano2, precoBPlano3} = precosBPlanos(calculoIMC);
-    const {melhorOP1, melhorOP2, melhorOP3} = compararPrecos(precoAPlano1, precoAPlano2, precoAPlano3, precoBPlano1, precoBPlano2, precoBPlano3);
-    exibirResultado(calculoIMC.toFixed(2), 
-    precoAPlano1.toFixed(0), 
-    precoAPlano2.toFixed(0), 
-    precoAPlano3.toFixed(0), 
-    precoBPlano1.toFixed(0), 
-    precoBPlano2.toFixed(0), 
-    precoBPlano3.toFixed(0),
-    melhorOP1.toFixed(0),
-    melhorOP2.toFixed(0),
-    melhorOP3.toFixed(0)); 
+    return {precoAPlano1, precoAPlano2, precoAPlano3}
+
 }
 
-function exibirResultado(calculoIMC, precoAPlano1, precoAPlano2, precoAPlano3, precoBPlano1, precoBPlano2, precoBPlano3, melhorOP1, melhorOP2, melhorOP3) {
-    document.getElementById('IMC').innerHTML = `IMC ${calculoIMC}`;
-    document.getElementById('planoA1').innerHTML = `PLANO A - BASICO R$ ${precoAPlano1}`;
-    document.getElementById('planoA2').innerHTML = `PLANO A - STANDARD R$ ${precoAPlano2}`;
-    document.getElementById('planoA3').innerHTML = `PLANO A - PREMIUM R$ ${precoAPlano3}`;
-    document.getElementById('planoB1').innerHTML = `PLANO B - BASICO R$ ${precoBPlano1}`;
-    document.getElementById('planoB2').innerHTML = `PLANO B - STANDARD R$ ${precoBPlano2}`;
-    document.getElementById('planoB3').innerHTML = `PLANO B - PREMIUM R$ ${precoBPlano3}`;
-    document.getElementById('melhorOP1').innerHTML = `R$ ${melhorOP1}`;
-    document.getElementById('melhorOP2').innerHTML = `R$ ${melhorOP2}`;
-    document.getElementById('melhorOP3').innerHTML = `R$ ${melhorOP3}`;
+function exibirResultado(melhorOP1, melhorOP2, melhorOP3) {
+    document.getElementById('melhorOP1').innerHTML = `R$ ${melhorOP1}<span class="spanMes">/mês</span>`;
+    document.getElementById('melhorOP2').innerHTML = `R$ ${melhorOP2}<span class="spanMes">/mês</span>`;
+    document.getElementById('melhorOP3').innerHTML = `R$ ${melhorOP3}<span class="spanMes">/mês</span>`;
 }
 
 function precosBPlanos (calculoIMC) {
@@ -67,7 +118,6 @@ function precosBPlanos (calculoIMC) {
     const precoBPlano1 = 100 + (comorbidade * 10 * (calculoIMC / 10));
     const precoBPlano2 = (150 + (comorbidade * 15)) * (calculoIMC / 10);
     const precoBPlano3 = (200 - (calculoIMC * 10) + (comorbidade * 20)) * (calculoIMC / 10);
-    console.log(comorbidade);
     return {precoBPlano1, precoBPlano2, precoBPlano3};
 }
 
@@ -98,22 +148,40 @@ function compararPrecos(precoAPlano1, precoAPlano2, precoAPlano3, precoBPlano1, 
 }
 
 function botaoBasico () {
-    alert("Você adquiriu o plano Básico. Obrigado pela preferência")
+    Swal.fire({
+        title: "Você adquiriu o plano Básico.",
+        text: "Obrigado pela preferência!",
+        confirmButtonColor: "#0d6efd"
+    });
 }
 
 function botaoStandard () {
-    alert("Você adquiriu o plano Standard. Obrigado pela preferência")
+    Swal.fire({
+        title: "Você adquiriu o plano Standard.",
+        text: "Obrigado pela preferência!",
+        confirmButtonColor: "#0d6efd"
+    });
 }
 
 function botaoPremium () {
-    alert("Você adquiriu o plano Premium. Obrigado pela preferência")
+    Swal.fire({
+        title: "Você adquiriu o plano Premium.",
+        text: "Obrigado pela preferência!",
+        confirmButtonColor: "#0d6efd"
+    });
 }
-  
+
+function transicao() {
+    document.querySelector('#area1').classList.add('fade1')
+    document.querySelector('#area2').classList.add('fade2')
+    document.querySelector('#area3').classList.add('fade3')
+    document.querySelector('#btn-calc-novamente').classList.add('fade4')
+
+}
+
 function pegarDados() {
     const peso = document.getElementById('peso').value;
     const altura = document.getElementById('altura').value;
     const idade = document.getElementById('idade').value;
     return { peso, altura, idade };
 }
-
-
